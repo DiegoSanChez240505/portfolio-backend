a6f47391d7d6c60c2fc58299f0ad61c17f5bb083
Diego Sanchez <eksanchezdiego24@example.com>
2025-04-29 11:24:44 -0600

    feat: Add @vitejs/plugin-react to package.json and remove gh-pages plugin from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
-import ghPages from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  base: '/portfolio-backend/', // ⚡ PON AQUÍ el nombre exacto de tu repo
-  plugins: [ghPages()],
+  base: '/portfolio-backend/',
 });
